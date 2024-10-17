3bfed4d53f181279b8b41f9616a31dbb6d7b1fed
nofate202 <156050859+nofate202@users.noreply.github.com>
2024-10-17 18:32:29 +0530

    Update AppTest.java

--- src/test/java/in/ashokit/AppTest.java
+++ src/test/java/in/ashokit/AppTest.java
@@ -1,8 +1,5 @@
 package in.ashokit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,38 +7,5 @@ public class AppTest {
     @Test
     void testSomething() {
         assertTrue(true); // Replace with actual test logic
-    }
-}
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
